307bf29887fc4014292a9f93288c6907312493e9
Cheesemcpuff <joshuahall98@live.com>
2024-11-17 11:21:08 +0000

    missing script issue on player panel prefab, resolved

--- DungeonBuilderGame/Assets/MyPackages/LocalMultiplayer/LobbyCreation/LocalPlayerLobbyPanel.cs
+++ DungeonBuilderGame/Assets/MyPackages/LocalMultiplayer/LobbyCreation/LocalPlayerLobbyPanel.cs
@@ -2,8 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using UnityEngine.InputSystem;
-using UnityEngine.InputSystem.UI;
 using UnityEngine.UI;
 
 public class LocalPlayerLobbyPanel : MonoBehaviour
